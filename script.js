38334e274b63301efa6a01763718ebec22b1e504
Roscoe Bull <roscoe-bull@live.co.uk>
2021-11-25 15:53:39 +0000

    fixed button bug, added styling for when its disabled

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const rollDie = document.querySelector("#die__roll")
 const rollDisplay = document.querySelector("#dice__display")
 const popupMessage = document.querySelector("#popup__message")
 
+
+//Creates the 100 divs that make up the game board
 const createGrid = () => {
    let divs = ""
    for (i=0; i<100; i++) {
@@ -13,10 +15,23 @@ const createGrid = () => {
 
 playGrid.innerHTML = createGrid();
 
+
+//default number for the die roll
 let totalRoll = 0;
 let previousTotalRoll=0;
 
+//sets player position to the start
+const startPosition = ()=>{
+   const g = document.getElementById("grid__0")
+   g.classList.add("player1")
+}
+
+startPosition()
+
+//die function that rolls the die and moves the player
 rollDie.addEventListener("click",()=> {
+   //disables roll button till function is complete
+   rollDie.setAttribute('disabled', true);
    previousTotalRoll=totalRoll
    const dieResult = Math.ceil(Math.random()*6)
    rollPicture(dieResult)
@@ -25,64 +40,77 @@ rollDie.addEventListener("click",()=> {
       totalRoll=99
       for (i=0;i<totalRoll-previousTotalRoll;i++){
          betweenPosition(previousTotalRoll+i,i)
-         console.log(previousTotalRoll+i)
-         console.log(totalRoll)
       }
       setTimeout(()=>{youWin()},(totalRoll-previousTotalRoll)*500)
    }else{
       for (i=0;i<totalRoll-previousTotalRoll;i++){
          betweenPosition(previousTotalRoll+i,i)
-         console.log(previousTotalRoll+i)
-         console.log(totalRoll)
       }
       setTimeout(()=>{ladderCheck(totalRoll)}, dieResult*500)
       setTimeout(()=>{snakeCheck(totalRoll)}, dieResult*500)
    }  
+   //renables button after movement of player has finished
+   setTimeout(()=>{rollDie.removeAttribute('disabled')}, dieResult*500)
 })
 
 
+
+//Edits image that appears for the die roll
 let oldDieRoll=-1;
 let dieImage=document.createElement('img')
+
+const dieDelete= () => {
+   document.querySelector("#dice__display").removeChild(dieImage)
+}
+
+const dieInsert = (number) =>{
+   dieImage = document.createElement('img')
+   dieImage.src = `./pictures/die${number}.PNG`
+   document.querySelector("#dice__display").appendChild(dieImage)
+}
+
 const rollPicture = (dieNumber) =>{
    if (oldDieRoll>0) {
-      document.querySelector("#dice__display").removeChild(dieImage)
-      dieImage = document.createElement('img')
-      dieImage.src = `./pictures/die${dieNumber}.PNG`
-      document.querySelector("#dice__display").appendChild(dieImage)
+      dieDelete()
+      dieInsert(dieNumber)
    }else{
-      dieImage = document.createElement('img')
-      dieImage.src = `./pictures/die${dieNumber}.PNG`
-      document.querySelector("#dice__display").appendChild(dieImage)
-      return oldDieRoll=`${dieNumber}`
+      dieInsert(dieNumber)
+      return oldDieRoll=dieNumber
    }
 }
 
-const startPosition = ()=>{
-   const g = document.getElementById("grid__0")
-   g.classList.add("player1")
-}
-
-startPosition()
-
+//You win message
 const youWin = ()=> {
    popupMessage.innerHTML = "YOU WIN!"
 }
+
+//========================
+//changing player position
+//========================
 
 const newPosition = (newNum)=>{
    const playerNew = document.getElementById(`grid__${newNum}`)
    playerNew.classList.add("player1")
 }
+
 const oldPosition = (oldNum)=>{
    const playerOld = document.getElementById(`grid__${oldNum}`)
    playerOld.classList.remove("player1")
 }
+
 const movePlayerPosition = (oldNum,newNum) =>{
    oldPosition(oldNum)
    newPosition(newNum)
 }
+
 const betweenPosition = (num,i) => {
    setTimeout(()=>{movePlayerPosition(num,num+1)}, i*500)
 }
+
+
+//==========================
+//==Snakes & ladders Check==
+//==========================
 
 const ladderCheck = (position) => {
    if (position===3){
@@ -105,6 +133,7 @@ const ladderCheck = (position) => {
    movePlayerPosition(71,89)
    }
 }
+
 const snakeCheck = (position) => {
    if (position===17){
    totalRoll=1,
